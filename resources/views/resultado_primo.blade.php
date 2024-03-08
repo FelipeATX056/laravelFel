cb3c8a8b4c58cf0dbe0ac5329a26a213714fcd41
Felipe <andresfelipegutierrezruiz@gmail.com>
2024-03-08 16:46:50 -0500

    primer commit

---
+++ resources/views/resultado_primo.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Resultado</title>
+</head>
+<body>
+    <h1>El número {{ $numero }} {{ $esPrimo ? 'es' : 'no es' }} primo.</h1>
+    <a href="/numero">Volver a intentar</a>
+    <li class="nav-item">
+        <a class="nav-link" href="pagina">VOLVER INICIO</a>
+      </li>
+      <button type="submit">Verificar</button>
+</body>
+</html>
